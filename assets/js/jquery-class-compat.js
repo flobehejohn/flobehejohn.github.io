25931fe30c97f8a6144d955cb37eefc2d59fae23
flobehejohn <florian.behejohn@hotmail.fr>
2026-04-24 22:07:00 +0200

    fix(contact): support legacy carousel helper

--- assets/js/jquery-class-compat.js
+++ assets/js/jquery-class-compat.js
@@ -54,6 +54,9 @@
     if (typeof api.tooltip !== 'function') {
       api.tooltip = function () { return api; };
     }
+    if (typeof api.carousel !== 'function') {
+      api.carousel = function () { return api; };
+    }
     var originalFind = api.find;
     if (typeof originalFind === 'function' && !api.__classFindCompat) {
       api.__classFindCompat = true;
