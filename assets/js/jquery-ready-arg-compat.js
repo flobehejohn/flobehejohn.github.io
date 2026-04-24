94b60fe8f70588781d509a8739fe37d52f4cc898
flobehejohn <florian.behejohn@hotmail.fr>
2026-04-24 15:43:18 +0200

    fix(contact): support legacy smartresize callback

--- assets/js/jquery-ready-arg-compat.js
+++ assets/js/jquery-ready-arg-compat.js
@@ -9,17 +9,24 @@
     else run();
   }
 
-  function compat(selector, root) {
-    if (typeof selector === 'function') {
-      onReady(selector, document);
-      return base([]);
-    }
-    var api = base(selector, root);
+  function addLegacyMethods(api) {
     api.ready = function (callback) {
       onReady(callback, document);
       return api;
     };
+    api.smartresize = function (callback) {
+      if (typeof callback === 'function') api.on('resize', callback);
+      return api;
+    };
     return api;
+  }
+
+  function compat(selector, root) {
+    if (typeof selector === 'function') {
+      onReady(selector, document);
+      return addLegacyMethods(base([]));
+    }
+    return addLegacyMethods(base(selector, root));
   }
 
   for (var key in base) compat[key] = base[key];
